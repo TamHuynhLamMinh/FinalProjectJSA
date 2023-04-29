936590d136c91d157dfff52144f2ad525c3f3312
TamHuynhLamMinh <student211132@ptnk.edu.vn>
2023-04-29 22:44:47 +0700

    hi

---
+++ js/data.js
@@ -0,0 +1,208 @@
+let movie = {
+    data: [
+        {
+            name: "Hunger",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABRYFcZB9BZx01c1S46-rHUmcwQSQWYCuhZW2WsxR3pj6xauaVjrr67mL2g_dCjYwdtnl-nfgVAHYNKM6NOF1A4dITrS8w03mT3jNwUiaPkUE9G_9ZJ0QJkCjW9w3tlTGtT9imw.jpg?r=938",
+            category: "original"
+        },
+
+        {
+            name: "13 Reasons Why",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABRDXroUCGLt0lKZxPqIfRH2GSgmhrlWRSGzpXiSdhGq2Th8W6JkBVp3KhEj3hI78NILPkpulgPh7Wnxms5JzTMkkCHqu1ZUq5xIpFL1HGDgjhbwe7HsAYK1H9fPsTzmLzGqZhA.jpg?r=579",
+            category: "original"
+        },
+
+        {
+            name: "Mine",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXfWhVjZ1mS92O2Oyh09nYAgq0DSXt3gNpqHw8yEbXL5OtNnVnekLmp3C6PVYPUmQlCBu1p34iNAIQ0f1Ero3q2Vd51sVDKNVhp7B687cb1XS0DHa0fFfW2640Rx27uCd3I99g.jpg?r=478",
+            category: "original"
+        },
+
+        {
+            name: "Law School",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABfVYow6ehaZl04Lu_yAjbIGAwjYQY4GsnJOzsEwTVD24ug-NScTQTkGy4KhTONMA5wwhAvJu1krNABIsdyVKIuakS8D879WTjoepE5JLLIFGGcp0g0WMmM_xuuW_HDPQz39G6A.jpg?r=171",
+            category: "original"
+        },
+
+        {
+            name: "Taxi Driver",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABfVYow6ehaZl04Lu_yAjbIGAwjYQY4GsnJOzsEwTVD24ug-NScTQTkGy4KhTONMA5wwhAvJu1krNABIsdyVKIuakS8D879WTjoepE5JLLIFGGcp0g0WMmM_xuuW_HDPQz39G6A.jpg?r=171",
+            category: "original"
+        },
+
+        {
+            name: "My Name",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABYsjIKF83oEnhuwI9n1JTDMAmG7RaIWIFn0j2m_xPEqHzyksNwqxnSRoRIO5jMmyM81hD9sd0FdLCKrsPyvpWt7V1UaxriOmpVOh.jpg?r=314",
+            category: "original"
+        },
+
+        {
+            name: "Remarriage & Desires",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWg0ZyRAB9Qd9_O5xyhorUOSsrRafbHUYnEu2Ssuc7rvBqD0Joxmm_XOj4zcm0dU_fHXDfr7ij-px6ODZCn6fd357rnqq3JT0CCnQ4QBku9TZhhictgIAgQ_hdT79hTqR4aL8Q.jpg?r=c71",
+            category: "original"
+        },
+
+        {
+            name: "Remarriage & Desires",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABT8BZhMxklmCrIiTP9aU2nRgjJOHRwOtrseHpbc921Jf7bJPBZtayk9q5R2l2YM8dwXv8x8Ov0jOO1yWADhH6obA-RJPvtOriaZY3rAcPHu7sz8Dgkn6AEz_Tudy8DKQ93geNg.jpg?r=e5b",
+            category: "original"
+        },
+
+        {
+            name: "Extracurricular ",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABb315saNYKTjwZ2iZ7x4BQhmmRlAlCeABGp6nuZhnz2o4IZqSgf1_h0D2GtsLBrp3M6he53nBwi86uylKVTthwDEnUKGEOvmBRBFjK6RPD-oa2eICtPd0oOaFP9jv3MIRX3l-g.jpg?r=1f9",
+            category: "original"
+        },
+
+        {
+            name: "Wednesday",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWAJSAFzlJdOuv3_snQnw4OH_1vJtwRT1OMdvTMm5gnR56Ng0Gxo6MUtSL6zq4zr32yxCfaDT2oDhwrnYhv6aUXXiyJ1189gchRTk677wNHIi1Nf2gYqVkcCqbENJPTbIewEBA.jpg?r=082",
+            category: "original"
+        },
+
+        {
+            name: "Sabrina",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWqis8Fb33LomFkzOoP36BsKPybFgu_UblglwNvoAp23an4F8OuMWdkj2l-q5HrnbQGo6S7uXP9gKfLZX4dbDhVwGDHGXwch2b1sC70Rw8_-G4gnOgktpHNm-eYPS67imQ4bxg.jpg?r=6f5",
+            category: "original"
+        },
+
+        {
+            name: "Enola Holmes",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABW6JB8l5MbuBKUGhXX9PrlFptRrcSr_e2GIq_xEqx0iGnfdwekczAeuYHm7nz6ZZPw311oSd7dJQnsC34uh0RhhGB9wBd1IlRiTTYeWgM7rFxzrFzSel_vMcamjY2i_4oZgmEQ.jpg?r=ffd",
+            category: "original"
+        },
+
+        {
+            name: "The School for Good and Evil",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQ7uINPt_UdhB5-iJ-D7qbgS4vGrFkTqBrxFCFpbZjNhWYeQhdQP7T1_L7VlkD1HIJDe2o6vt0f1XxmxMguC5n_g-uubqX5dhDxu8ujdXASAU6K3K9aEa3nnf_8EzSmoS58XgQ.jpg?r=aea",
+            category: "original"
+        },
+
+        {
+            name: "The Umbrella Academy",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWAMWJgf-kl6QDejmohK6xY2TMfbLeCP77pst4KTeWpMdoeGnvFCYpSjVBLbuSwVEcAIdwmgOna9xD7eXFlBVKsoHcMjXpFrbheH3NPIDpcDIL7kbWLO7k_M1N3cprXpJE_AYA.jpg?r=d60",
+            category: "original"
+        },
+
+        {
+            name: "Wendell & Wild",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUf0xHRnsgTYk8CU8FUAr7e0-RcodWFc1OSGhuVrHYuG2cRwctOTq7dF6xhXHCPfDOquIzP0QtVIpxw9VdnWOCdCUxWCgbbiJl2otHa2zO_0ep9YUWy3UHm7aIAYNYueS2nbkw.jpg?r=481",
+            category: "original"
+        },
+
+        {
+            name: "I Am Not Okay With This",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZ0WrQ582NBH8uM0ur3wCRStqB-pwsnyNEBBX7rb33mW1c4KF1eBBZV4O8LSEWC46tGoKtmbjgHcpfQN2W0KDgVzQBhww_onE3rNn6yvcvpvDCq0ROG0nUySz0RD0wgfMLURtw.jpg?r=052",
+            category: "original"
+        },
+
+        {
+            name: "Moxie",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXaKAloBq4605i-WaZK9W9aadcq_wNnum5tj7qTfmAHzAYpYBd2FNP7RymCT7ihABCN0TYIW3zJ3hvcpA77zb6Jz2NacJQxnxRBqMiz0UlznQNDCzTFlH8G7qDEpMxYSsMedYQ.jpg?r=48a",
+            category: "original"
+        },
+
+        {
+            name: "Spirited Away",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABSfHQxcoUhV-Hf227L9kYAHKaO1SSSF3zPVbj32Hms4So3BvkdIumM5sbI8JYqo9OCpOuNT_2Ba_T6PFNUYSs-h_rmPjc-4ylcPE.jpg?r=2e8",
+            category: "trending now"
+        },
+
+        {
+            name: "Ponyo",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUx6HB_65t256nQMM_ODXGgaHTFCEsfsO90nOpAjKm1kmTtRlnTb7QhLzOTnCJ4BoPfdsjAJqQ1HdgRNj6PY9GpNb2nZZOYVg6hn.jpg?r=597",
+            category: "trending now"
+        },
+
+        {
+            name: "Rilakkuma",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdbDx7uMSSGqTYyI7yjkFqpOLhN80_Kjik19JxUF9q5zAW37US63dTB5bLbU78W8xyER5fH82eshKl9gpwUCXQul6l0BA6bb3-rOKWpLGGcheuAwOYYrp8dHsUxnHQ2vX4c3Yw.jpg?r=c10",
+            category: "trending now"
+        },
+
+        {
+            name: "Pui Pui",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABae4Wlh5Y-IuDw3A-lylvtYnmLgLMTylhCwebiQdcjiK_Q51znbGRAP4AF0d_kGBNN9C4tiDJwLcWOg2feGIYF0kL0saA5Y2ybKh.jpg?r=010",
+            category: "trending now"
+        },
+
+        {
+            name: "My Neighbor TOTORO",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABRPjrT74N_Pe4uUZVfgjiEMl53EHEzFDCCnZnwn--GMdE6gIb04jGI3M-ETuqDbHsLtF-Lqii6mHfZdDglTUOpkRHvhXS0VduOOL.jpg?r=cc3",
+            category: "trending now"
+        },
+
+        {
+            name: "A Whisker Away",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXm7b9uZcD-nKIjCrn6lixm6qiJ1H__LxlfdwqK--E6EDaER1TDAB_y7Nb_DCR3iywbXXeTuLOdTwv_Kh2FOsYQqISsJ9VniRFB-R8yIJfpRz3hqYgquyfkWx7y7yuLq1JgjCw.jpg?r=76f",
+            category: "trending now"
+        },
+
+        {
+            name: "Stand By Me",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABYZkgnecM0y3l95vqxFHxCpRKSdrTOcJ3m_LR6WzFoIc2eqhvrsmF9ZoGfu2Z-6uXYkV1jZR5oYnDvVHIT4p-dJaw0x-t6x_QrWd.jpg?r=cc4",
+            category: "trending now"
+        },
+
+        {
+            name: "Storks",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZyKZoEC7J2n4-DOBpY3jZWSxPVbLzLqiKxHxlgbsu0k74u_r4gSrdUiJpQO2pSF32IwwMc8cx94IiHRlrGHeNjdwfySIcvfzh9s.jpg?r=a08",
+            category: "trending now"
+        },
+
+        {
+            name: "Storks",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZyKZoEC7J2n4-DOBpY3jZWSxPVbLzLqiKxHxlgbsu0k74u_r4gSrdUiJpQO2pSF32IwwMc8cx94IiHRlrGHeNjdwfySIcvfzh9s.jpg?r=a08",
+            category: "trending now"
+        },
+
+        {
+            name: "Howl's Moving Castle",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABaPGYc3hhUZ8qw-OitkCVwU_Zr7IhWOhfFLx76osQUXxpt7bJuI6DV-fPpsLrN5-_39ufUQOzV11V0ilNSG2DRUnY4G_s5z23K1j.jpg?r=ac4",
+            category: "top rated"
+        },
+
+        {
+            name: "KIKI's delivery service",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQT7HCO5v0gYU3aOepW1D-YiICU8a3GzvgDUgcK4uWjn-NkLR_68OpMdTXwkkYE8jidkiq4XfYBf1BiUgn-EogP9SBhfyPGps3ym.jpg?r=7c4",
+            category: "top rated"
+        },
+ 
+        {
+            name: "Arrietty",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXJKs7AvEVu-GvT-2-awd7K1BytOV8nVTTdFt6VlHiMXAqNVSmynrgA2obFKczamFOLnbveXbXwab4s4H-mSxkL4uBO26xJKyWS4.jpg?r=2b2",
+            category: "top rated"
+        },
+
+        {
+            name: "The Cat Returns",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABbUOLS0rC7JO96YQpS2FWydagcFU7Hm9BU6rtiyzPfxgXpr5RNN_cKUuMXvbkgsUZadsowlHzztFWY7ndkwep1NIccCourj2NAOc.jpg?r=521",
+            category: "top rated"
+        },
+
+        {
+            name: "Castle In The Sky",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABaCUixTV1EaJSt5bLc9JqTg-VBem6nDSyCmO9IxYTvqS6fOlOWZ7VNkHV_5SBA5t2I5UorcOyHDcbASV1WLwJHKo4ncahNT6GQnB.jpg?r=4ab",
+            category: "top rated"
+        },
+
+        {
+            name: "Whisper Of The Heart",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUfreg1VkrGvIX1ctMMB2V6p_3N_WSot5vHiubXUIYvcG0ywDyTnjAe5OUdeYflN26EZFoLgCs1G2581ClFppFuCbyk3eDurrTEi.jpg?r=4c8",
+            category: "top rated"
+        },
+
+        {
+            name: "Child Of Kamiari Month",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABfE78sg_SRslYMbIp7zqu65or2eiXtNciy8ICTwU4Mx5OzjoDrCuH6T4BPSdzanu9P2rEJ0xDZmsaDPDUMRNGtRWvwjVqdeEi2nKJnUPkzQVF9Dd5_1xtBWLLpyUNcq5sWKeKQ.jpg?r=053",
+            category: "top rated"
+        },
+
+        {
+            name: "SPYxFAMILY",
+            image: "https://occ-0-395-325.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABbSFuDgFOkbI0DtXHiwFVlKfGyN1ZEE-01NMelW8vI59aNjd53mTxJ9cVOlVgjW695Lie8azhuMTlcsiEtuqCnxydm3ubxlA1uBC.jpg?r=0c5",
+            category: "top rated"
+        },
+    ]
+}
+
